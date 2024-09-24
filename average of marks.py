87db5f269280ad8b7ce2873629080ba9e40063c3
22370048 <144317594+Shamsunnihar@users.noreply.github.com>
2024-09-24 15:13:32 +0530

    Add files via upload

---
+++ average of marks.py
@@ -0,0 +1,10 @@
+#Average of marks
+
+mark1 = float(input("Enter Physics mark:"))
+mark2 = float(input("Enter Maths mark:"))
+mark3 = float(input("Enter English mark:"))
+
+total_marks = mark1+mark2+mark3
+avg_marks = total_marks/3
+
+print(avg_marks)
